920c08ced1cc46b23ecf31c569d87c0ff0d10036
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-11-04 04:35:13 -0400

    test: Add more tests, namely for resolving services

--- tests/RemoraDelegateDispatch.Tests/DelegateMapResponderTests.cs
+++ tests/RemoraDelegateDispatch.Tests/DelegateMapResponderTests.cs
@@ -1,9 +1,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
 using Remora.Discord.API.Abstractions.Gateway.Events;
-using Remora.Discord.API.Abstractions.Objects;
 using Remora.Discord.Gateway.Extensions;
-using Remora.Discord.Gateway.Services;
+using Remora.Rest;
+using Remora.Results;
 using RemoraDelegateDispatch.Extensions;
 using RemoraDelegateDispatch.Services;
 
@@ -22,9 +22,10 @@ public class DelegateMapResponderTests
 
       services.AddDelegateResponder<IMessageCreate>
       (
-         (IMessageCreate _) =>
+         async (IMessageCreate _) =>
          {
             isSuccess = true;
+            return Result.FromSuccess();
          }
       );
 
@@ -34,5 +35,177 @@ public class DelegateMapResponderTests
       await service.RespondAsync(Substitute.For<IMessageCreate>());
 
       Assert.That(isSuccess);
+   }
+   
+   [Test]
+   public async Task DispatchesDelegateWithCancellationTokenCorrectly()
+   {
+      var services = new ServiceCollection();
+      services.AddDiscordGateway(_ => "");
+      services.AddDelegateResponders();
+
+      var isSuccess = false;
+      var ct = new CancellationToken(true);
+
+      services.AddDelegateResponder<IMessageCreate>
+      (
+         (IMessageCreate _, CancellationToken ct) =>
+         {
+            isSuccess = ct.IsCancellationRequested;
+         }
+      );
+
+      var provider = services.BuildServiceProvider();
+      var service = provider.GetRequiredService<DelegateMapResponder>();
+
+      await service.RespondAsync(Substitute.For<IMessageCreate>(), ct);
+
+      Assert.That(isSuccess);
+   }
+   
+   [Test]
+   public async Task DispatchesDelegateWithServiceCorrectly()
+   {
+      var services = new ServiceCollection();
+      services.AddDiscordGateway(_ => "");
+      services.AddDelegateResponders();
+
+      services.AddDelegateResponder<IMessageCreate>
+      (
+         (IMessageCreate _, IRestHttpClient _) => { }
+      );
+
+      Assert.DoesNotThrowAsync
+      (
+         async () =>
+         {
+            var provider = services.BuildServiceProvider();
+            var service = provider.GetRequiredService<DelegateMapResponder>();
+
+            await service.RespondAsync(Substitute.For<IMessageCreate>());
+         }
+      );
+   }
+   
+   [Test]
+   public async Task CoercesVoidDelegateCorrectly()
+   {
+      var services = new ServiceCollection();
+      services.AddDiscordGateway(_ => "");
+      services.AddDelegateResponders();
+
+      services.AddDelegateResponder<IMessageCreate>
+      (
+         (IMessageCreate _) => { } // Action<IMessageCreate>
+      );
+
+      Assert.DoesNotThrowAsync
+      (
+         async () =>
+         {
+            var provider = services.BuildServiceProvider();
+            var service = provider.GetRequiredService<DelegateMapResponder>();
+
+            await service.RespondAsync(Substitute.For<IMessageCreate>());
+         }
+      );
+   }
+   
+   [Test]
+   public async Task CoercesTaskDelegateCorrectly()
+   {
+      var services = new ServiceCollection();
+      services.AddDiscordGateway(_ => "");
+      services.AddDelegateResponders();
+
+      services.AddDelegateResponder<IMessageCreate>
+      (
+         async (IMessageCreate _) => { } // Func<IMessageCreate, Task>
+      );
+
+      Assert.DoesNotThrowAsync
+      (
+         async () =>
+         {
+            var provider = services.BuildServiceProvider();
+            var service = provider.GetRequiredService<DelegateMapResponder>();
+
+            await service.RespondAsync(Substitute.For<IMessageCreate>());
+         }
+      );
+   }
+   
+   [Test]
+   public async Task CoercesValueTaskDelegateCorrectly()
+   {
+      var services = new ServiceCollection();
+      services.AddDiscordGateway(_ => "");
+      services.AddDelegateResponders();
+
+      services.AddDelegateResponder<IMessageCreate>
+      (
+         ValueTaskMethod // Func<IMessageCreate, ValueTask>
+      );
+      
+      static ValueTask ValueTaskMethod(IMessageCreate _) => ValueTask.CompletedTask;
+
+      Assert.DoesNotThrowAsync
+      (
+         async () =>
+         {
+            var provider = services.BuildServiceProvider();
+            var service = provider.GetRequiredService<DelegateMapResponder>();
+
+            await service.RespondAsync(Substitute.For<IMessageCreate>());
+         }
+      );
+   }
+   
+   [Test]
+   public async Task CoercesResultTaskDelegateCorrectly()
+   {
+      var services = new ServiceCollection();
+      services.AddDiscordGateway(_ => "");
+      services.AddDelegateResponders();
+
+      services.AddDelegateResponder<IMessageCreate>
+      (
+         async (IMessageCreate _) => Result.FromSuccess() // Func<IMessageCreate, Task<Result>>
+      );
+
+      Assert.DoesNotThrowAsync
+      (
+         async () =>
+         {
+            var provider = services.BuildServiceProvider();
+            var service = provider.GetRequiredService<DelegateMapResponder>();
+
+            await service.RespondAsync(Substitute.For<IMessageCreate>());
+         }
+      );
+   }
+   
+   [Test]
+   public async Task HandlesComplexDelegateReturnCorrectly()
+   {
+      var services = new ServiceCollection();
+      services.AddDiscordGateway(_ => "");
+      services.AddDelegateResponders();
+
+      services.AddDelegateResponder<IMessageCreate>
+      (
+         async (IMessageCreate _) => Result<int>.FromSuccess(69) // Func<IMessage, Task<Result<T>>>
+      );
+
+      Assert.DoesNotThrowAsync
+      (
+         async () =>
+         {
+            var provider = services.BuildServiceProvider();
+            var service = provider.GetRequiredService<DelegateMapResponder>();
+
+            await service.RespondAsync(Substitute.For<IMessageCreate>());
+         }
+      );
    }
 }
